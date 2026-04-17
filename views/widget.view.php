7a93ca165d16e9ed08b05d218934f05e5d28a4f7
Alex V <vtorov.alex@gmail.com>
2026-04-17 15:00:50 +0300

    а

--- views/widget.view.php
+++ views/widget.view.php
@@ -13,10 +13,17 @@ $is_enabled = (bool) $data['is_enabled'];
 $state_label = $is_enabled ? _('Enabled') : _('Disabled');
 $state_class = $is_enabled ? 'state-enabled' : 'state-disabled';
 
-$toggle_button = (new CButton('trigger_toggle', $state_label))
+$toggle_button = (new CSubmit('trigger_toggle_submit', $state_label))
 	->addClass('js-trigger-toggle')
-	->addClass($state_class)
-	->setAttribute('data-toggle-action', $data['next_toggle_action']);
+	->addClass($state_class);
+
+$toggle_form = (new CForm('post', (new Curl('zabbix.php'))
+	->setArgument('action', 'widget.trigger_toggle_ors.toggle')
+	->getUrl()))
+	->addVar('toggle_action', $data['next_toggle_action'])
+	->addVar('triggerids', $data['triggerids_csv'])
+	->addVar('return_url', $_SERVER['HTTP_REFERER'] ?? '')
+	->addItem($toggle_button);
 
 $summary = (new CDiv([
 	(new CDiv([
@@ -34,7 +41,7 @@ $summary = (new CDiv([
 ]))->addClass('trigger-summary');
 
 $content = [
-	(new CDiv($toggle_button))->addClass('trigger-toggle-wrap')
+	(new CDiv($toggle_form))->addClass('trigger-toggle-wrap')
 ];
 
 if ($data['show_summary']) {
