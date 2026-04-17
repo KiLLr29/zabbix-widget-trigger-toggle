8bbd28cddbc6116e7f806f314e9e54cc2577fd66
Alex V <vtorov.alex@gmail.com>
2026-04-17 13:51:14 +0300

    update

--- assets/js/class.widget.js
+++ assets/js/class.widget.js
@@ -1,12 +1,25 @@
 class CWidgetTriggerToggleORS extends CWidget {
 
+    onInitialize() {
+        super.onInitialize();
+
+        this._is_edit_mode = false;
+        this._toggle_button = null;
+        this._content_container = null;
+    }
+
     onStart() {
         super.onStart();
 
+        this._content_container = this._target.querySelector('.trigger-toggle-widget');
         this._pending_toggle_action = null;
         this._events = {
             click: (event) => {
                 if (this._isDashboardEditMode()) {
+                    return;
+                }
+
+                if (!(event.target instanceof Element)) {
                     return;
                 }
 
@@ -25,16 +38,42 @@ class CWidgetTriggerToggleORS extends CWidget {
                 this._startUpdating();
             }
         };
+
+        this._bindToggleButton();
     }
 
     onActivate() {
         super.onActivate();
-        this._target.addEventListener('click', this._events.click);
+
+        if (this._content_container !== null) {
+            this._content_container.style.pointerEvents = this._isDashboardEditMode() ? 'none' : '';
+        }
+
+        this._bindToggleButton();
     }
 
     onDeactivate() {
-        this._target.removeEventListener('click', this._events.click);
+        this._unbindToggleButton();
+
         super.onDeactivate();
+    }
+
+    onEdit() {
+        if (typeof super.onEdit === 'function') {
+            super.onEdit();
+        }
+
+        this._is_edit_mode = true;
+        this._unbindToggleButton();
+
+        if (this._content_container !== null) {
+            this._content_container.style.pointerEvents = 'none';
+        }
+    }
+
+    processUpdateResponse(response) {
+        super.processUpdateResponse(response);
+        this._bindToggleButton();
     }
 
     getUpdateRequestData() {
@@ -49,10 +88,48 @@ class CWidgetTriggerToggleORS extends CWidget {
     }
 
     _isDashboardEditMode() {
+        if (this._is_edit_mode) {
+            return true;
+        }
+
+        if (typeof this.isEditMode === 'function') {
+            return this.isEditMode();
+        }
+
+        if (typeof this.getDashboard === 'function') {
+            const dashboard = this.getDashboard();
+
+            if (dashboard !== null && typeof dashboard.isEditMode === 'function') {
+                return dashboard.isEditMode();
+            }
+        }
+
         const body = document.body;
 
         return body.classList.contains('dashboard-is-edit-mode')
             || body.classList.contains('dashboard-edit-mode')
             || body.classList.contains('dashboard-mode-edit');
+    }
+
+    _bindToggleButton() {
+        this._unbindToggleButton();
+
+        if (this._isDashboardEditMode()) {
+            return;
+        }
+
+        const button = this._target.querySelector('.js-trigger-toggle');
+
+        if (button !== null) {
+            button.addEventListener('click', this._events.click);
+            this._toggle_button = button;
+        }
+    }
+
+    _unbindToggleButton() {
+        if (this._toggle_button !== null) {
+            this._toggle_button.removeEventListener('click', this._events.click);
+            this._toggle_button = null;
+        }
     }
 }
