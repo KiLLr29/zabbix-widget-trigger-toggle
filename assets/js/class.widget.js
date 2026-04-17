f3a0558063c6230b7c8e7bfaa8c8a5b661fbf1a6
Alex V <vtorov.alex@gmail.com>
2026-04-17 13:34:31 +0300

    change

--- assets/js/class.widget.js
+++ assets/js/class.widget.js
@@ -4,13 +4,15 @@ class CWidgetTriggerToggle extends CWidget {
         this._pending_toggle_action = null;
         this._events = {
             click: (event) => {
+                if (this._isDashboardEditMode()) {
+                    return;
+                }
+
                 const button = event.target.closest('.js-trigger-toggle');
 
                 if (!button) {
                     return;
                 }
-
-                event.preventDefault();
 
                 if (this._state !== WIDGET_STATE_ACTIVE) {
                     return;
@@ -40,5 +42,13 @@ class CWidgetTriggerToggle extends CWidget {
         }
 
         return update_request_data;
+    }
+
+    _isDashboardEditMode() {
+        const body = document.body;
+
+        return body.classList.contains('dashboard-is-edit-mode')
+            || body.classList.contains('dashboard-edit-mode')
+            || body.classList.contains('dashboard-mode-edit');
     }
 }
